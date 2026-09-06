20e22db313965f5fe91001522cfb27720eecedbc
Kasim Shah <Kasimshah998@gmail.com>
2026-09-06 14:20:26 +0530

    fix: remove invalid unused lucide-react import in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Instagram, Twitter, Facebook, DiscIcon as Discord } from "lucide-react";
+import { Instagram, Twitter, Facebook } from "lucide-react";
 
 export default function Footer() {
   return (
